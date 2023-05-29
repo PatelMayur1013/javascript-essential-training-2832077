8b563f411ea71112efcf473ea0c2f4545dc1f741
PatelMayur1013 <PatelMayur1013@users.noreply.github.com>
2023-05-29 17:32:43 +0000

    MTEST

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,25 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const car={
+    brand:"Hyndai",
+    model:"I10 Sports",
+    year :"2011",
+    color:"white",
+    size:{
+        height:500,
+        width:1000
+    },
+    isOn:false,
+    toggleOnOff: function(isOnOff)
+    {
+        this.isOn=isOnOff;
+    },
+    newsize:function(newheight,newwidth)
+    {
+        this.size.height=newheight;
+        this.size.width=newwidth;
+    }
+};
+
+console.log("Test",car);
